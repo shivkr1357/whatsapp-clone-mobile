c06f26b21c91d131761c6f7b06f530c8f2c1f467
Shiv Shankar Prasad <shivshankarkumar.pusa@gmail.com>
2024-09-09 15:10:50 +0530

    Initial Commit with Aithentication

--- App.tsx
+++ App.tsx
@@ -1,118 +1,83 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
+  Alert,
+  Button,
+  Dimensions,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
+import ReactNativeBiometrics from 'react-native-biometrics';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const height = Dimensions.get('screen').height;
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+const rnBiometrics = new ReactNativeBiometrics();
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [biometricsAvailable, setBiometricsAvailable] = useState(false);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  useEffect(() => {
+    // Check if biometrics are available
+    rnBiometrics
+      .isSensorAvailable()
+      .then(resultObject => {
+        const {available, biometryType} = resultObject;
+        if (available && biometryType) {
+          setBiometricsAvailable(true);
+        } else {
+          Alert.alert('Biometrics not available on this device');
+        }
+      })
+      .catch(err => {
+        console.log('Error: ', err);
+      });
+  }, []);
+
+  const authenticate = () => {
+    if (biometricsAvailable) {
+      rnBiometrics
+        .simplePrompt({promptMessage: 'Authenticate'})
+        .then(resultObject => {
+          const {success} = resultObject;
+          if (success) {
+            Alert.alert('Authentication successful');
+          } else {
+            Alert.alert('Authentication failed');
+          }
+        })
+        .catch(() => {
+          Alert.alert('Biometric prompt failed');
+        });
+    }
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView>
+      <View style={styles.logo}>
+        <Text>Whats App Clone Locked</Text>
+      </View>
+      <View style={styles.button}>
+        <Button title="Click to unlock" onPress={authenticate} />
+      </View>
     </SafeAreaView>
   );
 }
 
+export default App;
+
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  logo: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    top: 50,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  button: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    top: height / 2.5,
   },
 });
-
-export default App;
