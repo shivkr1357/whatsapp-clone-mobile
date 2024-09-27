e81bd81197fbda5088b03f916f4bff86e0bd5f15
Shiv Shankar Prasad <shivshankarkumar.pusa@gmail.com>
2024-09-27 07:43:14 +0530

    Changes

--- src/components/ViewedStatus/ViewedStauts.tsx
+++ src/components/ViewedStatus/ViewedStauts.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import {Avatar} from 'react-native-paper';
 
 const ViewedStauts = () => {
   return (
-    <View>
-      <Text>ViewedStauts</Text>
+    <View style={styles.container}>
+      <Text>Viewed Stauts</Text>
+      <View style={styles.statusContainer}>
+        <Avatar.Text label="SP" />
+      </View>
     </View>
   );
 };
 
 export default ViewedStauts;
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+  },
+  statusContainer: {
+    marginVertical: 20,
+  },
+});
